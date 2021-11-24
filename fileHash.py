68cb76e567f74616c7179e5d83158ece0a31fa69
rkuczer <ryan-kooz@hotmail.com>
2021-11-24 17:56:45 -0500

    more testing

--- fileHash.py
+++ fileHash.py
@@ -3,9 +3,19 @@ BLOCK_SIZE = 262144
 hash_handler = hashlib.sha3_512()
 file = open("hash.txt", "w")
 
-for i in range(0, 21, 1):
-    file_path = "/Users/ryankuczer/PycharmProjects/fileHashReal/files/f" + str(i) + ".docx" #file path with incrementing variable used to open file
+#master hash value
+file_path = "/Users/ryankuczer/PycharmProjects/fileHashReal/files/f0.docx" #file path with incrementing variable used to open file
+with open(file_path, 'rb') as f:
+    fb = f.read(BLOCK_SIZE)
+    while fb:
+        hash_handler.update(fb)
+        fb = f.read(BLOCK_SIZE)
+masterHash = hash_handler.hexdigest()
+print(f"{file_path} file hash value: \n{masterHash}\n")
 
+#other folder values
+for i in range(1, 21, 1):
+    file_path = "/Users/ryankuczer/PycharmProjects/fileHashReal/files/f" + str(i) + ".docx" #file path with incrementing variable used to open file
     with open(file_path, 'rb') as f:
         fb = f.read(BLOCK_SIZE)
         while fb:
@@ -13,13 +23,15 @@ for i in range(0, 21, 1):
             fb = f.read(BLOCK_SIZE)
     file_hexhash = hash_handler.hexdigest()
     file.write(file_hexhash+"\n")
-    print(f"{file_path} file hash value: \n{file_hexhash}")
+    print(f"{file_path} file hash value: \n{file_hexhash}\n")
 file.close()
-file=open("hash.txt", "r")
-line1 = file.readline()
+
+file1=open("hash.txt", "r")
+
 Num = 0
-for line in file:
-    if line == line1:
+
+for line in file1:
+    if line == masterHash:
         print("File matches original")
     else:
         print("File Number: " + str(Num) + ": Not matching")
