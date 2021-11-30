95b401265f20e95cffaa0d53a99f999c8201937d
rkuczer <ryan-kooz@hotmail.com>
2021-11-30 13:31:51 -0500

    more progress using arrays

--- fileHash.py
+++ fileHash.py
@@ -1,22 +1,15 @@
 import hashlib, sys
 BLOCK_SIZE = 512*512 #doubled blocked size from sha256
 hash_handler = hashlib.sha3_512()
+values = []
+#/Users/ryankuczer/PycharmProjects/fileHashReal/files/f0.docx
 
-#master hash value used to compare to examples in hash.txt
-file_path = "/Users/ryankuczer/PycharmProjects/fileHashReal/files/f0.docx" #file path with incrementing variable used to open file
-with open(file_path, 'rb') as f:
-    fb = f.read(BLOCK_SIZE)
-    while fb:
-        hash_handler.update(fb)
-        fb = f.read(BLOCK_SIZE)
-masterHash = hash_handler.hexdigest()
-#rint(f"Master hash is{file_path} file hash value: \n{masterHash}\n")
-
-file = open("hash.txt", "w")
-
-#for loop to find other values for f1-f20
-for i in range(1, 21, 1):
-    file_path = "/Users/ryankuczer/PycharmProjects/fileHashReal/files/f" + str(i) + ".docx" #file path with incrementing variable used to open file
+file = open("hash.txt", "w") #opens file to write hash values to
+path = input("Enter hash path to folder (including the last /): ") #gets file path for folder without actual file name in path
+#for loop to find other values for f0-f20
+for i in range(0, 21, 1):
+    file_path = path + "f" + str(i) + ".docx"
+    #file_path = "/Users/ryankuczer/PycharmProjects/fileHashReal/files/f" + str(i) + ".docx" #file path with incrementing variable used to open file
     hash_handler2 = hashlib.new('sha3_512')
     with open(file_path, 'rb') as f:
         fb = f.read(BLOCK_SIZE)
@@ -24,21 +17,32 @@ for i in range(1, 21, 1):
             hash_handler2.update(fb)
             fb = f.read(BLOCK_SIZE)
     file_hexhash = hash_handler2.hexdigest()
-    file.write(file_hexhash+"\n")
+    file.write(file_hexhash+"\n") #writes hex values to text file
+    values.append(file_hexhash) #appends value to array for comparison
     #print(f"{file_path} file hash value: \n{file_hexhash}\n")
 file.close()
 
 #open hash.txt and compare f0 to f1-f20
-file1=open("hash.txt", "rb")
-myline = file1.readline(64)
-print("Master hash: " + masterHash +"\n")
 
-while myline:
-    print(myline)
-    if myline == masterHash:
-        print("THIS FILE MATCHES")
-    else:
-        print("No matching file")
-    myline = file1.readline()
+print("Master hash value: " + values[0] +"\n")
+line = 0
+i = 0
+while i < 20:
+    line = line + 1
+    if values[0] == values[i]:
+        print("Found a matching file named f" + str(line) + ".docx")
+        print(values[i])
+        file1 = open("hash.txt", "a+")
+    i = i+1
 
 file1.close()
+
+#while myline:
+#    print(myline)
+#    if myline == masterHash:
+#        print("THIS FILE MATCHES")
+#    else:
+#        print("No matching file")
+#    myline = file1.readline()
+
+
